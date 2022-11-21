42203c6fe41f06adc12c566a37f73a4d49553db5
leetaewoo <taewoo5131@gmail.com>
2022-11-21 21:43:59 +0900

    도메인 분석 설계 - 엔티티 설계

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.OrderItem;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter
+@Setter
+public abstract class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @OneToMany(mappedBy = "item")
+    private List<OrderItem> orderItems;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+}
